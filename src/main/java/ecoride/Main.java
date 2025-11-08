5d2bd206c8074c06f66bf67cb21bfd6ed32e4d3f
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2025-11-08 19:21:15 +0530

    file added

---
+++ src/main/java/ecoride/Main.java
@@ -0,0 +1,11 @@
+package ecoride;
+
+/**
+ * Launcher for the EcoRide CLI app.
+ */
+public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.run();
+    }
+}
